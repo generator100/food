8e1e4c032da07df04c8a9a8310b62d2625a87bdc
generator <ericgne250@gmail.com>
2024-08-11 11:58:18 +0200

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) => {
 
 const [cartItems,setCartItems] = useState({})
 
-const url = "http://localhost:4000"
+const url = "https://backend-1-5f51.onrender.com"
 const [token,setToken] = useState("")
 const [food_list,setFoodList] = useState([])
 
